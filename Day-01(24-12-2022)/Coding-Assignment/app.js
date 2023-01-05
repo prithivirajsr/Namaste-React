02f512275bfa0228c3b146612968080a49ff6933
prithivirajsr <kingprithiviraj27@gmail.com>
2023-01-05 20:34:37 +0530

    day-01 coding-assignment done

---
+++ Day-01(24-12-2022)/Coding-Assignment/app.js
@@ -0,0 +1,10 @@
+// * Using Javascript
+
+const jscontainer = document.getElementById("js-container");
+const h1 = document.createElement("h1");
+h1.innerText = "Using Javascript";
+const h2 = document.createElement("h2");
+h2.innerText = "Namaste React";
+const h3 = document.createElement("h3");
+h3.innerText = "Hello world";
+jscontainer.append(h1, h2, h3);
